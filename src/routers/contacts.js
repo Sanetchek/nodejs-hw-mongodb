edeaa5c86dd60bfad59c03aaa2801bf5e371649c
Aleksandr <algryshko@gmail.com>
2024-09-09 12:23:54 +0300

    first part

---
+++ src/routers/contacts.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  getAllContactsController,
+  getContactByIdController
+} from "../controllers/contacts.js";
+import ctrlWrapper from "../utils/ctrlWrapper.js";
+
+
+const contactsRouter = Router();
+
+contactsRouter.get('/', ctrlWrapper(getAllContactsController));
+
+contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+export default contactsRouter;
